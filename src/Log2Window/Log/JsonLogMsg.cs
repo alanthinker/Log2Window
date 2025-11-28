85355ffcad297805fbbed676c6b9005a70495351
AlanThinker <alanthinker@126.com>
2025-11-28 13:38:40 +0800

    .

---
+++ src/Log2Window/Log/JsonLogMsg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log2Window.Log
+{
+    internal class JsonLogMsg
+    {
+        public string timestamp { get; set; }
+        public string level { get; set; }
+        public System.Text.Json.Nodes.JsonObject fields { get; set; }
+        public string target { get; set; }
+        public System.Text.Json.Nodes.JsonArray spans { get; set; }
+        public string threadId { get; set; }
+
+        public string ToMessage()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(fields["message"]?.GetValue<string>());
+
+            foreach (var field in fields)
+            {
+                if (field.Key != "message")
+                { sb.AppendLine(field.Key + " = " + field.Value); }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
